ea76eeff4bf59a7a14951bf5db861595c0d93123
GitHub Action <action@github.com>
2026-06-22 12:38:43 +0000

    Bump version to 1.22 (22)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.shreyash.dotrack"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.21"
+        versionCode = 22
+        versionName = "1.22"
 
         testInstrumentationRunner = "com.shreyash.dotrack.HiltTestRunner"
     }
